58a543a3be99e6b08661ed5b37c7703260cf54b4
Anh Dao <addao823@gmail.com>
2026-07-21 14:40:09 -0700

    Adding backend

---
+++ backend/src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import type { Express, Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const PORT: number = parseInt(process.env.PORT || '5000', 10);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/health', (_req: Request, res: Response) => {
+    res.json({
+        status: 'Ok', message: 'Backend is running'
+    });
+});
+
+app.get('/api/test', (_req: Request, res: Response) => {
+    res.json({
+        message: 'Hello World!'
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
