9e7e808bfb24809115eb9b1e5316b000dcc2c52c
Anh Dao <addao823@gmail.com>
2026-08-25 10:55:37 -0700

    Adding soft delete

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
   const loadTasks = async () => {
     try {
       setLoading(true)
-      const data = await taskDb.getAll()
+      const data = await taskDb.getActive()
       data.sort((a, b) => b.create_date - a.create_date)
       setTaskList(data)
     } catch (error) {
@@ -63,6 +63,7 @@ function App() {
       is_done: false,
       create_date: Date.now(),
       update_date: Date.now(),
+      is_delete: 0,
       synced: 0,
     }
 
@@ -78,7 +79,17 @@ function App() {
 
   const onDelete = async (taskId: string) => {
     try {
-      await taskDb.delete(taskId)
+      let taskObj: ClientTask | undefined = taskList.find(
+        (t) => t.id === taskId,
+      )
+      if (!taskObj) throw Error("Can't find the task with id")
+      const update = {
+        ...taskObj,
+        is_delete: 1,
+        synced: 0,
+        update_date: Date.now(),
+      }
+      await taskDb.save(update)
       setTaskList((prev) => prev.filter((t) => t.id !== taskId))
     } catch (error) {
       console.log('Failed to delete task:', error)
@@ -93,8 +104,9 @@ function App() {
       if (!taskObj) throw Error("Can't find the task with id")
       const update = {
         ...taskObj,
-        isDone: !taskObj.is_done,
-        updatedAt: Date.now(),
+        is_done: !taskObj.is_done,
+        synced: 0,
+        update_date: Date.now(),
       }
       await taskDb.save(update)
       setTaskList((prev) => prev.map((t) => (t.id === update.id ? update : t)))
