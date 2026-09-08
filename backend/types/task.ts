e5388b527600c677601e701bd6e022dcf7f69318
Anh Dao <addao823@gmail.com>
2026-09-08 12:50:09 -0700

    adding server types

---
+++ backend/types/task.ts
@@ -0,0 +1,35 @@
+export interface ServerTask {
+    id: string
+    client_id: string
+    user_id: string
+    label: string
+    is_done: boolean
+    is_delete: boolean
+    create_date: Date
+    update_date: Date
+    delete_at?: Date
+    synced: boolean
+    version: number
+}
+
+export interface TaskInput {
+    client_id: string
+    label: string
+    is_done: boolean
+    is_delete: boolean
+    create_date: number
+    update_date: number
+    delete_at?: number
+}
+
+export interface SyncTaskInput {
+    client_id: string
+    server_id?: string
+    text: string
+    is_done: boolean
+    is_delete: boolean
+    create_date: number
+    update_date: number
+    delete_at?: number
+}
+
